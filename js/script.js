2389502035a99101bbf494b7b95403b521188e35
Danceny Karla <dancenykarla@gmail.com>
2024-06-12 01:08:25 +0200

    js DOM

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
  // Aquí tu código
+
+ const btnAgregar = document.getElementById("agregar");
+ const contenedorLista = document.getElementById("lista");
+
+ btnAgregar.addEventListener("click", function(){
+    const elementoListado = prompt("Introduce el nombre del país o ciudad");  
+    const destino = document.createElement("li");
+    destino.innerText = elementoListado; 
+    contenedorLista.appendChild(destino);
+ })
+
+
+
+
+
